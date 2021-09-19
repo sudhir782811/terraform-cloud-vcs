5a58df804f41a32887d7f37254930d0e7a352fe3
sudhir782811 <sudhir782811@gmail.com>
2021-09-19 10:53:30 +0530

    1st commit

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "my_web" {
   ami = var.my_ami
   key_name = "rk"
   instance_type = var.ec2_type["big-app"]
-  region = "eu-central-1"
   tags = {
     Name = "my_web"
     env = "dev"
